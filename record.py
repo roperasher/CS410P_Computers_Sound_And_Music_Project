15cc594ea2aa3e746bf4cb3c98c18d4450900e88
Asher Roper <roper@pdx.edu>
2020-05-11 14:32:21 -0700

    record audio for 6 seconds then output .wav file

--- record.py
+++ record.py
@@ -5,7 +5,7 @@ chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
-seconds = 6 
+seconds = 6  # Duration of recording
 filename = "output.wav"
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
